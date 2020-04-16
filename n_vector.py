0211c663ec63052843292346f5c21565ff48c4f6
NikNo280 <60934033+NikNo280@users.noreply.github.com>
2020-04-16 19:24:01 +0300

    Update n_vector.py

--- n_vector.py
+++ n_vector.py
@@ -4,16 +4,14 @@ class N_Vector:
 
     def __init__(self, string):
         self._dimension = []
-        self._size = 0;
-        for item in string.split(' '):
-            self._dimension.append(int(item))
-            self._size += 1
+        self._size = 0
+        try:
+            for item in string.split(' '):
+                self._dimension.append(int(item))
+                self._size = self._size + 1
+        except ValueError:
+            self._dimension = []
 
-    def get_size(self):
-        return self._size
-
-    def get_dimension(self):
-        return self._dimension
 
     def multiplication_by_constant(self, const):
         self._dimension = list(map(lambda x : x * const, self._dimension))
@@ -28,32 +26,26 @@ class N_Vector:
     def get_item(self, index):
         return self._dimension[index]
 
+    def __add__(self, vector2):
+        return list(map(lambda x, y : x + y, self._dimension, vector2._dimension))
 
-class Operations:
+    def __sub__(self, vector2):
+        return list(map(lambda x, y : x - y, self._dimension, vector2._dimension))
 
-    def __init__(self, string1, string2):
-        self.vector1 = N_Vector(string1)
-        self.vector2 = N_Vector(string2)
+    def __mul__(self, vector2):
+        return list(map(lambda x, y: x * y, self._dimension, vector2._dimension))
 
-    def sum(self):
-        return list(map(lambda x, y : x + y, self.vector1._dimension, self.vector2._dimension))
+    def __truediv__(self, vector2):
+        return list(map(lambda x, y: x / y, self._dimension, vector2._dimension))
 
-    def subtraction(self):
-        return list(map(lambda x, y : x - y, self.vector1._dimension, self.vector2._dimension))
+    def __str__(self):
+        return '{}'.format(self._dimension)
 
-    def multiplication(self):
-        return list(map(lambda x, y: x * y, self.vector1._dimension, self.vector2._dimension))
+    def __eq__(self, vector2):
+        return True if self._dimension == vector2._dimension else False
 
-    def division(self):
-        return list(map(lambda x, y: x / y, self.vector1._dimension, self.vector2._dimension))
-
-    def scalar_product(self):
+    def scalar_product(self, vector2):
         sum = 0
-        for item in list(map(lambda x, y: x * y, self.vector1._dimension, self.vector2._dimension)):
+        for item in list(map(lambda x, y: x * y, self._dimension, vector2._dimension)):
            sum += item
         return sum
-    def equals(self):
-        if self.vector1._dimension == self.vector2._dimension:
-            return True
-        return False
-
