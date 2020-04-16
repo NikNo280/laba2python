e33d3372bb7ff52e942aa7313c4e35cd9f313b6a
NikNo280 <60934033+NikNo280@users.noreply.github.com>
2020-04-16 19:27:47 +0300

    Update test_to_json.py

--- test_to_json.py
+++ test_to_json.py
@@ -1,82 +1,65 @@
 import unittest
 from to_json import JsonFormat
-
-class Student:
-    def __init__(self, name, age, average_mark, person_name, person_age):
-        self.name = name
-        self.age = age
-        self.average_mark = average_mark
-        self.person = Person(person_name, person_age)
-
-
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+import json
 
 
 class TestJson(unittest.TestCase):
 
     def setUp(self):
         self.converter = JsonFormat()
-        self.student = Student(
-            name="Vasia",
-            age=18,
-            average_mark=7.2,
-            person_name="lala",
-            person_age=322
-        )
 
-    def test_list_to_string(self):
-        self.assertTrue(self.converter.to_json([1, 2.0, '3', True, False, None, self.student])
-                        == "[ 1, 2.0, '3', True, False, None, { 'Student':{ 'name':'Vasia', 'age':18, 'average_mark':7.2, 'person':{ 'Person':{ 'name':'lala', 'age':322 } } } } ]",
-                        "Неверный формат list")
-        self.assertTrue(self.converter.to_json(self.student)
-                        == "{ 'Student':{ 'name':'Vasia', 'age':18, 'average_mark':7.2, 'person':{ 'Person':{ 'name':'lala', 'age':322 } } } }",
-                        "Неверный формат list")
-        self.assertFalse(self.converter.to_json([1, 2.0, '3', True, False, None, self.student])
-                        == "[ 1, 2.0, '3', True, False, None, { 'Stsdfdudent':{ 'name':'Vasia', 'age':18, 'average_mark':7.2, 'person':{ 'Person':{ 'name':'lala', 'age':322 } } } } ]",
-                        "Неверный формат list")
+    def test_int(self):
+        data = 10
+        self.assertEqual(json.dumps(data), self.converter.to_json(data))
 
-    def test_dict_to_string(self):
-        self.assertTrue(self.converter.to_json({'1' : 2.0, '3' : True, 4 : None, '5' :self.student})
-                        == "{ '1':2.0, '3':True, '4':None, '5':{ 'Student':{ 'name':'Vasia', 'age':18, 'average_mark':7.2, 'person':{ 'Person':{ 'name':'lala', 'age':322 } } } } }",
-                        "Неверный формат dict")
-        self.assertFalse(self.converter.to_json({'1' : 2.0, '3' : True, 4 : None, '5' :self.student})
-                         == "{ '1':2.0, '3':True, '4':None, '5':{ 'Studensat':{ 'name':'Vasia', 'age':18, 'average_mark':7.2, 'person':{ 'Person':{ 'name':'lala', 'age':322 } } } } }",
-                         "Неверный формат dict")
+    def test_float(self):
+        data = 9.9
+        self.assertEqual(json.dumps(data), self.converter.to_json(data))
 
-    def test_class_to_dict(self):
-        self.assertTrue(self.converter.to_json(self.student)
-                        == "{ 'Student':{ 'name':'Vasia', 'age':18, 'average_mark':7.2, 'person':{ 'Person':{ 'name':'lala', 'age':322 } } } }",
-                        "Неверный формат class")
-        self.assertFalse(self.converter.to_json(self.student)
-                         == "{ 'Student':{ 'nafsame':'Vasia', 'age':18, 'average_mark':7.2, 'person':{ 'Person':{ 'name':'lala', 'age':322 } } } }",
-                         "Неверный формаsaт class")
+    def test_none(self):
+        data = None
+        self.assertEqual(json.dumps(data), self.converter.to_json(data))
 
-    def test__dict_format(self):
-        self.assertTrue(self.converter._dict_format("abc", 3)
-                        == "'abc':3",
-                        "Неверный формат key/value")
-        self.assertFalse(self.converter._dict_format("abc", 3)
-                        == "'abc' : 4",
-                        "Неверный формат key/value")
+    def test_str(self):
+        data = 'hello'
+        self.assertEqual(json.dumps(data), self.converter.to_json(data))
 
-    def test_normal_type_to_string(self):
-        self.assertTrue(self.converter._normal_type_to_string(None)
-                        == "None",
-                        "Неверный формат normal_type")
-        self.assertFalse(self.converter._normal_type_to_string(True)
-                        == "true",
-                        "Неверный формат normal_type")
+    def test_tuple(self):
+        data = (10, 10)
+        self.assertEqual(json.dumps(data), self.converter.to_json(data))
 
-    def test_to_json(self):
-        self.assertTrue(self.converter.to_json([1, 2.0, '3', True, False, None, self.student])
-                        == "[ 1, 2.0, '3', True, False, None, { 'Student':{ 'name':'Vasia', 'age':18, 'average_mark':7.2, 'person':{ 'Person':{ 'name':'lala', 'age':322 } } } } ]",
-                        "Неверный формат list")
-        self.assertFalse(self.converter.to_json({'1': 2.0, '3': True, 4: None, '5': self.student})
-                         == "{ '1':2.0, '3':True, '4':None, '5':{ 'Studensat':{ 'name':'Vasia', 'age':18, 'average_mark':7.2, 'person':{ 'Person':{ 'name':'lala', 'age':322 } } } } }",
-                         "Неверный формат dict")
+    def test_tuple_diff(self):
+        data = ('10', 10, 9.9, (9, 10), None, 110)
+        self.assertEqual(json.dumps(data), self.converter.to_json(data))
+
+    def test_list(self):
+        data = [10, 9]
+        self.assertEqual(json.dumps(data), self.converter.to_json(data))
+
+    def test_list_diff(self):
+        data = ['10', 9, 9.9, (9, 9)]
+        self.assertEqual(json.dumps(data), self.converter.to_json(data))
+
+    def test_dict(self):
+        data = {10: 10, 9: 9}
+        self.assertEqual(json.dumps(data), self.converter.to_json(data))
+
+    def test_dict_str(self):
+        data = {'10': '10', '10': '9'}
+        self.assertEqual(json.dumps(data), self.converter.to_json(data))
+
+    def test_dict_diff(self):
+        data = {'10': '10', 2: '2', 3: None}
+        self.assertEqual(json.dumps(data), self.converter.to_json(data))
+
+    def test_dict_adn_list(self):
+        data = {'10': [3, 2, None], 2: '2', 3: False}
+        self.assertEqual(json.dumps(data), self.converter.to_json(data))
+
+    def test_list_adn_dict(self):
+        data = [{'10': '10', 2: '2', 3: True}]
+        self.assertEqual(json.dumps(data), self.converter.to_json(data))
+
 
 if __name__ == '__main__':
     unittest.main()
