c963720f0cd03d7a9d988057a5e8f0bae2ef41c7
NikNo280 <60934033+NikNo280@users.noreply.github.com>
2020-04-16 19:29:16 +0300

    Update test_cached.py

--- test_cached.py
+++ test_cached.py
@@ -1,11 +1,31 @@
-import cathed
+from cathed import cached
+import math
 import unittest
+from time import time, sleep
 
 class TestCached(unittest.TestCase):
 
     def test_cached(self):
-        self.assertTrue(cathed.add(1, 2) == 3, "Ошибка в сложении")
-        self.assertFalse(cathed.add(1, 2) == 4, "Ошибка в сложении")
+
+        @cached
+        def factorial(n):
+            if n < 0:
+                return 0
+            elif n < 2:
+                return 1
+            else:
+                result = 1
+                for value in range(2, int(n) + 1):
+                    result *= value
+            return result
+
+        start = time()
+        factorial(1736)
+        first_time = time() - start
+        start = time()
+        factorial(1736)
+        second_time = time() - start
+        self.assertGreater(first_time, second_time, "Cached execution error")
 
 if __name__ == '__main__':
     unittest.main()
